5ae9add4a21c79c57a7dd8495c499ac8ce64861d
jorgito22 <j.c.rojasm@hotmail.com>
2015-05-13 19:41:27 -0400

    Se agrego el codigo para dibujar el Nivel

--- js/Nivel.js
+++ js/Nivel.js
@@ -1,12 +1,24 @@
-function Nivel(elementos,fondo, nombre, distancia_actual,){
+function Nivel(elementos,fondo, nombre, distancia_actual, x, y, w, h){
   this.elementos = elementos;
   this.fondo = fondo;
   this.nombre = nombre;
   this.distancia_actual = distancia_actual
-  this.dibujar = function() {
-    this.fondo.dibujar;
-    for (var i = elementos.length - 1; i >= 0; i--) {
-      elementos[i].dibujar();
-    };
-  };
+  this.x = x;
+  this.y = y;
+  this.w = w;
+  this.h = h;
+  this.dibujar = function(context){
+      context.strokeRect(this.x, this.y, this.w, this.h);
+      var x_actual= 0;
+      for (var i=0; i < elementos.length ; i++) {
+        var elemento = elementos[i];
+        elemento.dibujar(context, this.x + x_actual);
+        x_actual += elemento.w+1;
+
+      };
+
+      //dividir el ancho en 500 partes
+
+      //dividir la altura en 10 partes iguales
+  }
 };
